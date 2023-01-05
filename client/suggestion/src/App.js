5830b1438fb5f3ec30215746fdb23e6ff310a411
koechcoding <koechkelvin2001@gmail.com>
2023-01-05 13:24:00 +0300

    client side

--- client/suggestion/src/App.js
+++ client/suggestion/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios'
+import React from 'react';
 
 function App() {
+  //Adding state to get the mobile names from db
+  const[mobileNames,setMobileNames] = React.useState([])
+
+  //With use effect set the mobile names post getting it from database
+  React.useEffect(() =>{
+    Axios.get('http://localhost:3001/api/get/mobileMake').then((response) =>{
+      let data = response.data
+      let finalArray = []
+      //Storing values in the form of array as response will be in the form of objects
+      finalArray = data.map(function(obj){
+        return obj.mobileName
+      })
+      console.log(finalArray)
+      setMobileNames([...finalArray])
+  })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input name='mobile' placeholder='mobileMake' list='suggestion'></input>
+     <datalist id='suggestion'>
+     {mobileNames.map((make,index) =>{
+      //Parsing the array and displaying suggestion with option tag
+        return (<option key={index} value={make}>{make}</option>)
+       })}
+     </datalist>
     </div>
   );
 }
